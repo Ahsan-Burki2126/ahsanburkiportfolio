99fc2aec09874fd274efb67ff41ac7dc0e4d49cb
Ahsan Burki <ahsanburki2126@gmail.com>
2026-04-16 01:58:57 +0500

    No words

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActionsBodySizeLimit: "10mb",
+  },
+};
 
 module.exports = nextConfig;
